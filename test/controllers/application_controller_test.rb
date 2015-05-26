ecfd3ec7bcd14a6db52a2e9a31fe6e6227d431b0
Joseph L <josephtwli@berkeley.edu>
2015-05-25 22:19:26 -0700

    Hid hello world, compressed tests into 1 tests in order to minimize the number of times the twitter API is queried.

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -2,15 +2,14 @@ require 'test_helper'
 
 class ApplicationControllerTest < ActionController::TestCase
 
-  test "should get title" do
+  # The following test is really three tests in 1 so as to limit the number of
+  # times I query the Twitter API (since there's a tight allownace on how often I can do that).
+  test "should get title, div, h1" do
     get :hello
     assert_response :success
     assert_select "title", "TrueX"
+    assert_select "div", "hello, world!"
+    assert_select "h1", "Recent tweets by trueX"
   end
 
-  test "should get 'hello, world!'" do
-    get :hello
-    assert_response :success
-    assert_select "div", "hello, world!"
-  end
 end
