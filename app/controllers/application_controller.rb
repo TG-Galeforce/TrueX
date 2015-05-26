159a31d099b03d76947a71897c2c19ceabdd3e9e
Joseph L <josephtwli@berkeley.edu>
2015-05-26 15:56:25 -0700

    Caching now works as it should.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,6 +22,44 @@ class ApplicationController < ActionController::Base
 		return timeline		
 	end
 
+	def get_info(client, user_id)
+		timeline = get_timeline(client, user_id)
+		puts "foo"
+		tl_p = []
+		for tweet in timeline
+			u_mentions = tweet.user_mentions
+
+			tweet_processed = {}
+			tweet_text = tweet.text
+
+			text_split = []
+			last_ind = 0
+			for m in u_mentions
+				inds = m.indices
+				w1 = tweet_text[last_ind..inds[0]-1]
+
+				c_user = client.user(m.id)
+				followers_cnt = c_user.followers_count
+				statuses_cnt = c_user.statuses_count
+				title_text = '" title="' + statuses_cnt.to_s + ' tweets, ' + followers_cnt.to_s + ' followers' + '">'
+
+				w2 = '<a href="/users/' + m.id.to_s + title_text + tweet_text[inds[0]..inds[1]-1] + '</a>' #The name
+				text_split.append(w1)
+				text_split.append(w2)
+				last_ind = inds[1]
+			end
+			wlast = tweet_text[last_ind..-1]
+			text_split.append(wlast)
+
+			text_processed = text_split.join
+
+			tweet_processed['text'] = text_processed
+			tweet_processed['created_at'] = tweet.created_at;
+			tl_p.append(tweet_processed)
+		end
+		timeline_processed = tl_p
+	end
+
 	def user_tweets
 		user_id = 202759325
 		if params[:uid]
@@ -29,43 +67,10 @@ class ApplicationController < ActionController::Base
 		end
 
 		client = init_client
-		# expires_in 5.minutes, :public => false
-		# timeline_processed = Rails.cache.fetch(:timeline_processed) do
-			timeline = get_timeline(client, user_id)
-			tl_p = []
-			for tweet in timeline
-				u_mentions = tweet.user_mentions
-
-				tweet_processed = {}
-				tweet_text = tweet.text
-
-				text_split = []
-				last_ind = 0
-				for m in u_mentions
-					inds = m.indices
-					w1 = tweet_text[last_ind..inds[0]-1]
-
-					c_user = client.user(m.id)
-					followers_cnt = c_user.followers_count
-					statuses_cnt = c_user.statuses_count
-					title_text = '" title="' + statuses_cnt.to_s + ' tweets, ' + followers_cnt.to_s + ' followers' + '">'
-
-					w2 = '<a href="/users/' + m.id.to_s + title_text + tweet_text[inds[0]..inds[1]-1] + '</a>' #The name
-					text_split.append(w1)
-					text_split.append(w2)
-					last_ind = inds[1]
-				end
-				wlast = tweet_text[last_ind..-1]
-				text_split.append(wlast)
-
-				text_processed = text_split.join
-
-				tweet_processed['text'] = text_processed
-				tweet_processed['created_at'] = tweet.created_at;
-				tl_p.append(tweet_processed)
-			end
-			timeline_processed = tl_p
-		# end
+		expires_in 5.minutes, :public => false
+		timeline_processed = Rails.cache.fetch(user_id) do
+			timeline_processed = get_info(client, user_id)
+		end
 
 		@tweets = timeline_processed
 		@uid = user_id
